5514ba13a116e01b362eec55216f9877b0738a1c
Shameek Ganguly <shameekg@stanford.edu>
2017-11-27 17:28:46 -0800

    Added haptic spring simulation

--- 01-branch/main.cpp
+++ 01-branch/main.cpp
@@ -5,15 +5,19 @@
 #include "QuadraticSplineKinematic.h"
 #include "QuadraticSplineVisual.h"
 
+#include "timer/LoopTimer.h"
+
 #include <Eigen/Core>
 #include <iostream>
 #include <string>
+#include <thread>
 
 using namespace std;
 using namespace Eigen;
+using namespace chai3d;
 
 const string world_file = "resources/01-branch/world.urdf";
-const string camera_name = "camera_isometric";
+const string camera_name = "camera_front";
 
 // callback to print glfw errors
 void glfwError(int error, const char* description);
@@ -30,6 +34,23 @@ bool fTransXn = false;
 bool fTransYp = false;
 bool fTransYn = false;
 bool fRotPanTilt = false;
+
+// function for updating haptics
+bool fSimulationRunning = false;
+void updateHaptics(
+	cGenericHapticDevicePtr device,
+	chai3d::cShapeSphere* cursor,
+	chai3d::cShapeLine* haptic_force_line,
+	Sai2Graphics::Sai2Graphics* graphics,
+	QuadraticSplineKinematic* spline,
+	QuadraticSplineVisual* spline_graphic,
+	chai3d::cShapeSphere* cherry,
+	const double s_cherry
+);
+
+// flags for haptic interaction
+bool fHapticDeviceEnabled = false;
+bool fHapticSwitchPressed = false;
 
 int main(int argc, char** argv) {
 	cout << "Loading URDF world model file: " << world_file << endl;
@@ -95,6 +116,36 @@ int main(int argc, char** argv) {
 	glfwSetKeyCallback(window, keySelect);
 	glfwSetMouseButtonCallback(window, mouseClick);
 
+	/*------- Set up haptic interaction -------*/
+	// create a sphere (cursor) to represent the haptic device
+    auto cursor = new cShapeSphere(0.1);
+    // create a line to show the haptic force
+    auto haptic_force_line = new cShapeLine();
+    haptic_force_line->setShowEnabled(false);
+    haptic_force_line->setLineWidth(4.0);
+		
+	// create a haptic device handler
+    auto handler = new cHapticDeviceHandler();
+
+	// get a handle to the first haptic device
+    cGenericHapticDevicePtr hapticDevice;
+	handler->getDevice(hapticDevice, 0);
+	if (NULL == hapticDevice) {
+		cout << "No haptic device found. " << endl;
+		fHapticDeviceEnabled = false;
+	} else {
+		hapticDevice->open();
+		hapticDevice->calibrate();
+		fHapticDeviceEnabled = true;
+		// if the device has a gripper, enable the gripper to simulate a user switch
+	    hapticDevice->setEnableGripperUserSwitch(true);
+	    graphics->_world->addChild(cursor);
+	    graphics->_world->addChild(haptic_force_line);
+	}
+
+	thread haptics_thread(updateHaptics, hapticDevice, cursor, haptic_force_line, graphics, spline, spline_graphic, cherry, s_cherry);
+
+	/*------- Loop -------*/
 	// cache variables
 	double last_cursorx, last_cursory;
 
@@ -102,57 +153,20 @@ int main(int argc, char** argv) {
 	Eigen::Matrix3d R;
 	Eigen::Vector3d center_point = Eigen::Vector3d::Zero();
 
-	// cherry position
-	Vector3d cherry_pos_local;
-
-	// spline dynamics variables
-	double ks = 0.15;
-	double b = 0.05;
-	double dt = 0.01;
-	double cherry_r = r0;
-	double cherry_r_max = 0.25;
-	const double cherry_growth_rate = 0.02; // r/ sec
-	MatrixXd Jv_s;
-	MatrixXd Jlp;
-	VectorXd dq(2);
-	Vector3d F_cherry;
-	VectorXd gamma_cherry;
-	const double density_cherry = 0.15;
-
 	// while window is open:
     while (!glfwWindowShouldClose(window))
 	{
 		// update graphics. this automatically waits for the correct amount of time
 		int width, height;
 		glfwGetFramebufferSize(window, &width, &height);
-		
-		/* --- SPLINE KINEMATICS UPDATES BEG --- */
-		spline->splineLocation(cherry_pos_local, s_cherry);
-		cherry->setLocalPos(cherry_pos_local);
 
-		if (cherry_r < cherry_r_max) {
-			cherry_r += cherry_growth_rate*dt;
-		}
-		cherry->setRadius(cherry_r);
-		/* --- SPLINE KINEMATICS UPDATES END --- */
-
+		// render scene
 		graphics->render(camera_name, width, height);
 
-
-		/* --- SPLINE DYNAMICS BEG ---*/
-		spline->splineLinearJacobian(Jv_s, s_cherry);
-		F_cherry.setZero();
-		F_cherry[2] = -9.8*density_cherry*4.0/3.0*M_PI*pow(cherry_r,3);
-		gamma_cherry = Jv_s.transpose()*F_cherry;
-
-		spline->splineTipProjectionLengthJacobian(Jlp);
-		dq[0] = -Jlp(0,0)*ks/b*spline->splineTipProjectionLength() + gamma_cherry[0]/b;
-		dq[1] = -Jlp(0,1)*ks/b*spline->splineTipProjectionLength() + gamma_cherry[1]/b;
-		spline->_alpha += dq[0]*dt;
-		spline->_beta += dq[1]*dt;
+		// compute global position of spline and cherry
+		cherry->computeGlobalPositions();
+		spline_graphic->computeGlobalPositions();
 		
-		/* --- SPLINE DYNAMICS END ---*/
-
 		// swap buffers
 		glfwSwapBuffers(window);
 
@@ -213,6 +227,10 @@ int main(int argc, char** argv) {
 
 	}
 
+	// stop simulation
+	fSimulationRunning = false;
+	haptics_thread.join();
+	
     // destroy context
     glfwDestroyWindow(window);
 
@@ -220,6 +238,170 @@ int main(int argc, char** argv) {
     glfwTerminate();
 
 	return 0;
+}
+
+//------------------------------------------------------------------------------
+void updateHaptics(
+	cGenericHapticDevicePtr hapticDevice,
+	chai3d::cShapeSphere* cursor,
+	chai3d::cShapeLine* haptic_force_line,
+	Sai2Graphics::Sai2Graphics* graphics,
+	QuadraticSplineKinematic* spline,
+	QuadraticSplineVisual* spline_graphic,
+	chai3d::cShapeSphere* cherry,
+	const double s_cherry
+) {
+	// create a timer
+	LoopTimer timer;
+	timer.initializeTimer();
+	timer.setLoopFrequency(1000); //1000Hz timer
+	double last_time = timer.elapsedTime(); //secs
+
+	bool fTimerDidSleep = true;
+
+	// cherry updates
+	Vector3d cherry_pos_local, cherry_pos_global;
+	bool cherry_still_on_plant = true;
+	const double cherry_pluck_force_thresh = 1.0;
+	Vector3d cherry_vel, cherry_acc, cherry_force;
+
+	// spline dynamics variables
+	double ks = 0.75;
+	double b = 0.1;
+	double dt = 0.01;
+	double cherry_r = cherry->getRadius();
+	double cherry_r_max = 0.25;
+	const double cherry_growth_rate = 0.007; // r/ sec
+	MatrixXd Jv_s;
+	MatrixXd Jlp;
+	VectorXd dq(2);
+	Vector3d F_cherry;
+	VectorXd gamma_cherry;
+	const double density_cherry = 0.65;
+
+	// haptics device
+	cVector3d position;
+	const double scale_factor = 100.0;
+	Vector3d home_pos(-0.25, 0.0, 1.5);
+	Vector3d F_haptic;
+	double last_distance;
+	Vector3d last_dir;
+	const double haptic_kp = 0.6;
+	const double haptic_force_scale = 9.0;
+	const double haptic_kv = 0.01;
+
+	// start simulation loop
+	fSimulationRunning = true;
+	while (fSimulationRunning) { //automatically set to false when simulation is quit
+		fTimerDidSleep = timer.waitForNextLoop();
+
+		// update time
+		double curr_time = timer.elapsedTime();
+		double loop_dt = curr_time - last_time;
+
+		/* --- SPLINE KINEMATICS UPDATES BEG --- */
+		if (cherry_still_on_plant) {
+			spline->splineLocation(cherry_pos_local, s_cherry);
+			cherry->setLocalPos(cherry_pos_local);
+
+			if (cherry_r < cherry_r_max) {
+				cherry_r += cherry_growth_rate*dt;
+			}
+			cherry->setRadius(cherry_r);
+		}
+		/* --- SPLINE KINEMATICS UPDATES END --- */
+
+		if (fHapticDeviceEnabled) {
+			// haptics updates
+	        hapticDevice->getPosition(position);
+	        cursor->setLocalPos(position*scale_factor + home_pos);
+
+	        hapticDevice->getUserSwitch(0, fHapticSwitchPressed);
+
+	        if (fHapticSwitchPressed) {
+	        	// ignore if the cursor is too far
+	        	if (
+	        		(cursor->getLocalPos().eigen() - cherry->getGlobalPos().eigen()).norm() > (cherry->getRadius()*2.0 + cursor->getRadius())
+	        		&& !haptic_force_line->getShowEnabled()
+        		) {
+	        		fHapticSwitchPressed = false;
+	        	} else {
+		        	haptic_force_line->m_pointA = cherry->getGlobalPos();
+		        	haptic_force_line->m_pointB = cursor->getLocalPos();
+		        	haptic_force_line->setShowEnabled(true);
+
+		        	// compute forces
+		        	double distance = (haptic_force_line->m_pointB - haptic_force_line->m_pointA).eigen().norm();
+		        	if (distance > 1e-2) {
+		        		Vector3d dir = (haptic_force_line->m_pointB - haptic_force_line->m_pointA).eigen().normalized();
+			        	F_haptic = haptic_kp * dir * distance;
+			        	if (!cherry_still_on_plant) {
+			        		F_haptic -= haptic_kv * (distance - last_distance)/dt * dir;
+			        	}
+			        	last_dir = dir;
+		        	} else {
+		        		F_haptic.setZero();
+		        		if (!cherry_still_on_plant) {
+			        		F_haptic -= haptic_kv * (distance - last_distance)/dt * last_dir;
+			        	}
+		        	}
+		        	last_distance = distance;
+	        	}
+			}
+			if (!fHapticSwitchPressed) {
+				F_haptic.setZero();
+				haptic_force_line->setShowEnabled(false);
+	        }
+		}
+
+		/* --- CHERRY DYNAMICS BEG ---*/
+		if (F_haptic.norm() > cherry_pluck_force_thresh) {
+			// remove cherry from plant and add it to tree
+			cherry_pos_global = cherry->getGlobalPos().eigen();
+			spline_graphic->removeChild(cherry);
+			graphics->_world->addChild(cherry);
+			cherry->setLocalPos(cherry_pos_global);
+			// set flag
+			cherry_still_on_plant = false;
+			cherry_vel.setZero();
+		}
+
+		if (!cherry_still_on_plant) {
+			cherry_pos_global = cherry->getGlobalPos().eigen();
+			cherry_acc.setZero();
+			cherry_acc[2] -= 9.8;
+			cherry_acc += 1.0/(density_cherry*4.0/3.0*M_PI*pow(cherry_r,3))*F_haptic;
+			cherry_vel += cherry_acc*0.01;
+			cherry_pos_global += cherry_vel*0.01;
+			cherry->setLocalPos(cherry_pos_global);
+		}
+
+		/* --- CHERRY DYNAMICS END ---*/
+
+		/* --- SPLINE DYNAMICS BEG ---*/
+		spline->splineLinearJacobian(Jv_s, s_cherry);
+		F_cherry.setZero();
+		if (cherry_still_on_plant) {
+			F_cherry[2] = -9.8*density_cherry*4.0/3.0*M_PI*pow(cherry_r,3);
+			F_cherry += F_haptic;
+		}
+		gamma_cherry = Jv_s.transpose()*F_cherry;
+
+		spline->splineTipProjectionLengthJacobian(Jlp);
+		dq[0] = -Jlp(0,0)*ks/b*spline->splineTipProjectionLength() + gamma_cherry[0]/b;
+		dq[1] = -Jlp(0,1)*ks/b*spline->splineTipProjectionLength() + gamma_cherry[1]/b;
+		spline->_alpha += dq[0]*dt;
+		spline->_beta += dq[1]*dt;
+
+		if (fHapticDeviceEnabled) {
+			// apply haptics forces
+			hapticDevice->setForceAndTorqueAndGripperForce(-cVector3d(F_haptic)*haptic_force_scale, Vector3d(0.0, 0.0, 0.0), 0.0);
+		}
+
+		// -------------------------------------------
+		// update last time
+		last_time = curr_time;
+	}
 }
 
 //------------------------------------------------------------------------------
