69b55d38ad406baf1ca2d910895cdcd7284547c2
Shameek Ganguly <shameekg@stanford.edu>
2017-12-11 11:54:30 -0800

    Added python implementations for some kinematic functions required for contact

--- scripts/derivative_tests.py
+++ scripts/derivative_tests.py
@@ -175,6 +175,32 @@ def Ryz_dalpha(s, alp, beta):
 def Rzy_dalpha(s, alp, beta):
 	return Ryz_dalpha(s, alp, beta)
 
+def dR_dalpha(s, alp, beta):
+	arr = np.zeros((3,3))
+	arr[0,0] = Rxx_dalpha(s, alp, beta)
+	arr[1,0] = Rxy_dalpha(s, alp, beta)
+	arr[2,0] = Rxz_dalpha(s, alp, beta)
+	arr[0,1] = Ryx_dalpha(s, alp, beta)
+	arr[1,1] = Ryy_dalpha(s, alp, beta)
+	arr[2,1] = Ryz_dalpha(s, alp, beta)
+	arr[0,2] = Rzx_dalpha(s, alp, beta)
+	arr[1,2] = Rzy_dalpha(s, alp, beta)
+	arr[2,2] = Rzz_dalpha(s, alp, beta)
+	return arr
+
+def dR_dbeta(s, alp, beta):
+	arr = np.zeros((3,3))
+	arr[0,0] = Rxx_dbeta(s, alp, beta)
+	arr[1,0] = Rxy_dbeta(s, alp, beta)
+	arr[2,0] = Rxz_dbeta(s, alp, beta)
+	arr[0,1] = Ryx_dbeta(s, alp, beta)
+	arr[1,1] = Ryy_dbeta(s, alp, beta)
+	arr[2,1] = Ryz_dbeta(s, alp, beta)
+	arr[0,2] = Rzx_dbeta(s, alp, beta)
+	arr[1,2] = Rzy_dbeta(s, alp, beta)
+	arr[2,2] = Rzz_dbeta(s, alp, beta)
+	return arr
+
 def Rxx_dbeta(s, alp, beta):
 	l = 1.0
 	psi = np.arcsin(np.cos(alp)*np.cos(beta))
@@ -275,18 +301,47 @@ def lp_beta(s, alp, beta):
 	ret_val *= s*s/2.0/l*gam*(sinc(s*gam/2.0/l)**2)
 	return ret_val
 
-# test
+def closest_s_from_point(alp, beta, point):
+	l = 1.0
+	psi = np.arcsin(np.cos(alp)*np.cos(beta))
+	gam = np.pi - 2.0*psi
+	sa = np.sin(alp)
+	sb = np.sin(beta)
+	ca = np.cos(alp)
+	cb = np.cos(beta)
+	unit_line = np.array([ca*cb, sb, -sa*cb])
+	n = np.cross(np.array([1.0, 0.0, 0.0]), unit_line)
+	n = n/np.linalg.norm(n)
+	y = np.cross(n, np.array([1.0, 0.0, 0.0]))
+	p_proj = point - np.dot(point, n)*n
+	# print gam*p_proj[0], l - gam*np.dot(y, p_proj), l/gam*np.arctan2(gam*p_proj[0], l - gam*np.dot(y, p_proj))
+	sp = l/gam*np.arctan2(gam*p_proj[0], l - gam*np.dot(y, p_proj))
+	if sp < 0 or sp > l:
+		# check end points
+		dist1 = np.linalg.norm(p_proj);
+		spline_end = np.array([ca*cb, sb, -sa*cb])*l*sinc(gam/2.0)
+		dist2 = np.linalg.norm(p_proj - spline_end)
+		if dist1 < dist2:
+			sp = 0
+		else:
+			sp = l
+	return sp
+
+# # test
 # alp = np.pi/10.0
 # beta = np.pi/10.0
 # s = 0.732
 # print Jv_s(s, alp, beta)
+# print dR_dalpha(s, alp, beta)
+# print dR_dbeta(s, alp, beta)
 
-alp = np.linspace(-np.pi*2.0/3.0, np.pi*2.0/3.0, 20)
-beta = np.linspace(-np.pi*2.0/3.0, np.pi*2.0/3.0, 20)
+alp = np.linspace(-np.pi*1.0/3.0, np.pi*1.0/3.0, 20)
+beta = np.linspace(-np.pi*1.0/3.0, np.pi*1.0/3.0, 20)
 grad = np.zeros((20,20))
 for i in range(20):
 	for j in range(20):
-		grad[i,j] = lp_beta(0.5, alp[i], beta[j])
+		# grad[i,j] = lp_beta(0.5, alp[i], beta[j])
+		grad[i,j] = closest_s_from_point(alp[i], beta[j], np.array([-0.0, 0.0, 0.6]))
 
 plt.imshow(grad, cmap='hot', interpolation='nearest')
 plt.colorbar()
